52fa784ffda144a51681f99cac2c81679314f601
PkTheCoda <pranavkonjeti@gmail.com>
2023-08-20 19:20:59 -0400

    added logo and fixed header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
 import logo from '../Images/pklogo.png'
+import pranavtextlogo from '../Images/pranavlogotext.png'
 
 const navigation = [
   { name: 'About', href: '#about' },
@@ -20,7 +21,11 @@ export default function Navbar() {
             <div className="flex lg:flex-1">
               <a href="#" className="-m-1.5 p-1.5">
                 <span className="sr-only">Your Company</span>
-                <div className='font-outfit font-bold'>Pranav Konjeti</div>
+                <img
+                    className="h-8 w-auto"
+                    src={pranavtextlogo}
+                    alt=""
+                  />
               </a>
             </div>
             <div className="flex lg:hidden">
@@ -46,7 +51,7 @@ export default function Navbar() {
               </a>
             </div>
           </nav>
-          <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
+          <Dialog as="div" className="lg:hidden font-outfit" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
             <div className="fixed inset-0 z-50" />
             <Dialog.Panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
               <div className="flex items-center justify-between">
@@ -54,7 +59,7 @@ export default function Navbar() {
                   <span className="sr-only">Your Company</span>
                   <img
                     className="h-8 w-auto"
-                    src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                    src={pranavtextlogo}
                     alt=""
                   />
                 </a>
@@ -85,7 +90,6 @@ export default function Navbar() {
                       href="#"
                       className="-mx-3 block rounded-lg px-3 py-2.5 text-base  leading-7 text-gray-900 hover:bg-gray-50"
                     >
-                      Log in
                     </a>
                   </div>
                 </div>
