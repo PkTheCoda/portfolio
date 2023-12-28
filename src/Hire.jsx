a6fdc871fb87a76c1313c746c8d338384835db97
PkTheCoda <pranavkonjeti@gmail.com>
2023-12-27 23:17:36 -0500

    worked on hire page

--- src/Hire.jsx
+++ src/Hire.jsx
@@ -1,10 +1,14 @@
 import HireHeader from "./HireComponents/HireHeader"
+import HireFirst from "./HireComponents/HireFirst"
+import HireTimeline from "./HireComponents/HireTimeline"
 
 
 export default function Hire() {
     return (
         <>
             <HireHeader />
+            <HireFirst />
+            <HireTimeline />
             <div className="bg-red-100">hi</div>
         </>
     )
