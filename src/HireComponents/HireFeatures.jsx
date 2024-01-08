50e2352ac70c76b7e30dc6cf9453fc26ff3c7d8c
PkTheCoda <pranavkonjeti@gmail.com>
2024-01-07 20:42:34 -0500

    worked on /hire grid

--- src/HireComponents/HireFeatures.jsx
+++ src/HireComponents/HireFeatures.jsx
@@ -1,79 +1,67 @@
 import React from 'react';
-import hireSpeed from '../Images/hireSpeed.svg'
+import hiregrid1 from '../Images/hiregrid1.svg'
+import hiregrid2 from '../Images/hiregrid2.svg'
+import hiregrid3 from '../Images/hiregrid3.svg'
+import hiregrid4 from '../Images/hiregrid4.svg'
 
 const HireFeatures = () => {
-  return (
-    <div class="bg-white dark:bg-gray-800 h-full py-6 sm:py-8 lg:py-12">
-    <div class="mx-auto max-w-screen-2xl px-4 md:px-8">
-        <div class="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
-            <div class="flex items-center gap-12">
-                <h2 class="text-2xl font-bold text-gray-800 lg:text-3xl dark:text-white">Gallery</h2>
-
-                <p class="hidden max-w-screen-sm text-gray-500 dark:text-gray-300 md:block">
-                    This is a section of some simple filler text,
-                    also known as placeholder text. It shares some characteristics of a real written text.
-                </p>
-            </div>
-
-            <a href="#"
-                class="inline-block rounded-lg border bg-white dark:bg-gray-700 dark:border-none px-4 py-2 text-center text-sm font-semibold text-gray-500 dark:text-gray-200 outline-none ring-indigo-300 transition duration-100 hover:bg-gray-100 focus-visible:ring active:bg-gray-200 md:px-8 md:py-3 md:text-base">
-                More
-            </a>
-        </div>
-
-        <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 md:gap-6 xl:gap-8">                
-
-          <div class="p-5 gap-y-4 flex flex-col justify-center items-center font-semibold h-48 overflow-hidden rounded-lg border md:h-80 bg-[#fafaf9]">
-
-            <img src={hireSpeed} alt="hireSpeed" />
-
-            <div className='font-outfit text-4xl'>
-              <span className="main-gradient">Blazing</span> fast websites. 
-            </div>
-
+    return (
+      <div className='flex flex-col gap-y-8 px-60 pb-60'>
+          <div className="text-7xl font-semibold font-outfit">
+              <span className="main-gradient">Everything you need</span> from a website.
           </div>
-
-            <a href="#"
-                class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80">
-                <img src="https://images.unsplash.com/photo-1542759564-7ccbb6ac450a?auto=format&q=75&fit=crop&w=1000" loading="lazy" alt="Photo by Magicle" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-
-                <div
-                    class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+  
+            <div className="w-full font-outfit flex bg-[#fbeee6] rounded-xl overflow-hidden"> {/* Add overflow-hidden here */}
+                <div className="w-1/2 flex flex-col gap-y-8 justify-center px-10">
+                    <div className='text-5xl font-semibold text-gray-900'>Your Website deserves to be at the top 👑</div>
+                    <div className="text-gray-700 text-2xl tracking-wide">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum quis quae explicabo nesciunt perspiciatis, illo nemo soluta voluptatibus commodi ab nobis eligendi quo ea nam esse ipsa accusamus. Aut, itaque.</div>
                 </div>
-
-                <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Tech</span>
-            </a>
-            {/* <!-- image - end -->
-
-            <!-- image - start --> */}
-            <a href="#"
-                class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:col-span-2 md:h-80">
-                <img src="https://images.unsplash.com/photo-1610465299996-30f240ac2b1c?auto=format&q=75&fit=crop&w=1000" loading="lazy" alt="Photo by Martin Sanchez" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
-
-                <div
-                    class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                <div className='w-1/2 justify-end flex'>
+                    <img src={hiregrid1} alt="" className="w-full relative -bottom-10 -right-10" />
                 </div>
+            </div>
 
-                <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Dev</span>
-            </a>
-            {/* <!-- image - end -->
 
-            <!-- image - start --> */}
-            <a href="#"
-                class="group relative flex h-48 items-end overflow-hidden rounded-lg bg-gray-100 shadow-lg md:h-80">
-                <img src="https://images.unsplash.com/photo-1550745165-9bc0b252726f?auto=format&q=75&fit=crop&w=600" loading="lazy" alt="Photo by Lorenzo Herrera" class="absolute inset-0 h-full w-full object-cover object-center transition duration-200 group-hover:scale-110" />
+            <div className="w-full gap-x-8 font-outfit flex rounded-xl overflow-hidden"> {/* Add overflow-hidden here */}
+                <div className="w-1/2 flex flex-col gap-y-8 bg-[#f7f7f7] rounded-xl py-10">
+                    <div className='px-10 flex flex-col gap-y-4'>
+                        <div className='text-6xl font-semibold text-gray-900'>Blazing Fast Speed</div>
+                        <div className="text-gray-700 text-2xl tracking-wide">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum quis quae explicabo nesciunt perspiciatis, illo nemo soluta voluptatibus commodi ab nobis eligendi quo ea nam esse ipsa accusamus. Aut, itaque.
+                        </div>
+                    </div>
+                    <img src={hiregrid2} alt="" className="w-full rounded-xl px-10" />
+                    <div className="text-gray-500 text-xl tracking-wide px-10">
+                        ****While I can't <i>guaruantee</i> perfect web scores, I can <b>guaruantee</b> that your website will load 
+                        fast and flawlessly regardless of device constraints and whatnot!
 
-                <div
-                    class="pointer-events-none absolute inset-0 bg-gradient-to-t from-gray-800 via-transparent to-transparent opacity-50">
+                    
+                    </div>
                 </div>
+                <div className="w-1/2 flex flex-col gap-y-8 bg-[#f7f7f7] rounded-xl py-10">
+                    <div className='px-10 flex flex-col gap-y-4'>
+                        <div className='text-6xl font-semibold text-gray-900'>Instant Response Time</div>
+                        <div className="text-gray-700 text-2xl tracking-wide">
+                            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum quis quae explicabo nesciunt perspiciatis, illo nemo soluta voluptatibus commodi ab nobis eligendi quo ea nam esse ipsa accusamus. Aut, itaque.
+                        </div>
+                    </div>
+                    <img src={hiregrid3} alt="" className="w-full rounded-xl px-10" />
+                </div>
+            </div>
 
-                <span class="relative ml-4 mb-3 inline-block text-sm text-white md:ml-5 md:text-lg">Retro</span>
-            </a>
-            {/* <!-- image - end --> */}
-        </div>
-    </div>
-</div>
-  );
-};
+            <div className="w-full font-outfit flex bg-[#fbeee6] rounded-xl overflow-hidden"> {/* Add overflow-hidden here */}
+                <div className="w-1/2 flex flex-col gap-y-8 justify-center px-10">
+                    <div className='text-5xl font-semibold text-gray-900'>Responsive down to the corner 🔬</div>
+                    <div className="text-gray-700 text-2xl tracking-wide">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum quis quae explicabo nesciunt perspiciatis, illo nemo soluta voluptatibus commodi ab nobis eligendi quo ea nam esse ipsa accusamus. Aut, itaque.</div>
+                </div>
+                <div className='w-1/2 justify-end flex'>
+                    <img src={hiregrid4} alt="" className="w-full relative " />
+                </div>
+            </div>
+
+
+      </div>
+    );
+  };
 
 export default HireFeatures;
