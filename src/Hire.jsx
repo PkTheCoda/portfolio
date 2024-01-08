e3393d04ce564a1d57bc079220a1826fdc651ebe
PkTheCoda <pranavkonjeti@gmail.com>
2024-01-07 22:11:15 -0500

    added final hire cta section + second footer page

--- src/Hire.jsx
+++ src/Hire.jsx
@@ -3,6 +3,8 @@ import HireFirst from "./HireComponents/HireFirst"
 import HireTimeline from "./HireComponents/HireTimeline"
 import HireFeatures from "./HireComponents/HireFeatures"
 import HireFinalCTA from "./HireComponents/HireFinalCTA"
+import ContactSection from "./Components/ContactSection"
+import WavyLine from "./Components/WavyLine"
 
 
 export default function Hire() {
@@ -13,6 +15,8 @@ export default function Hire() {
             <HireTimeline />
             <HireFeatures />
             <HireFinalCTA />
+            <WavyLine />
+            <ContactSection />
         </>
     )
 }
--- src/HireComponents/HireFinalCTA.jsx
+++ src/HireComponents/HireFinalCTA.jsx
@@ -3,9 +3,14 @@ export default function HireFinalCTA() {
       <>
         <div className="relative px-40 flex justify-center mb-20">
           {/* Add the shadow blob */}
-          <div className="flex gap-x-20 absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-            <div className="p-40 bg-blue-500 opacity-40 rounded-full blur-3xl">hi</div>
-            <div className="p-40 bg-blue-200 opacity-40 rounded-full blur-3xl">hi</div>
+          <div className="flex gap-x-20 absolute z-10 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 blur-3xl">
+          <div
+              className="relative aspect-[1155/678] w-[40rem] bg-gradient-to-tr from-[#de8c52] from-30% to-[#4133d8] via-[#d21d23] opacity-50 "
+              style={{
+                clipPath:
+                  'polygon(12.58% 16.91%, 24.76% 10.52%, 36.95% 16.91%, 47.44% 10.52%, 66.68% 6.99%, 76.59% 16.91%, 84.33% 16.91%, 88.89% 37.91%, 91.64% 60.12%, 83.35% 62.08%, 76.59% 71.88%, 66.93% 72.68%, 57.43% 76.31%, 47.04% 81.23%, 38.05% 69.73%, 23.1% 76.31%, 18.66% 63.16%, 9.36% 53.87%, 5.81% 28.54%)',
+              }}
+            />
           </div>
   
           <div className="bg-[#171717] text-white text-center w-full text-5xl font-semibold rounded-xl py-20 flex flex-col gap-y-4 items-center font-outfit px-20 relative ">
